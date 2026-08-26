7c0b1ba3bd7e16bd6eef8099d1f14d833cf18fe8
JavierCardenasAguirre <javiertrader1979@outlook.com>
2026-08-26 14:58:03 -0500

    precios de rebaja

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ const products = [
     {
         id: 1,
         name: 'Cases Fibra',
-        price: 25000,
-        originalPrice: 17500,
+        price: 17500,
+        originalPrice: 25000,
         rating: 4,
         reviews: 19,
         image: 'images/fibra.webp',
